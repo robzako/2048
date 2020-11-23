2153cd825b71188e675e43759cc7ca4927ea66f1
Rob Zako <robzako@gmail.com>
2020-11-22 23:17:37 -0800

    Update game_manager.js
    Note GameManager() called by "application.js"

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,3 +1,4 @@
+// Called by "application.js"
 function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
   this.inputManager   = new InputManager;
